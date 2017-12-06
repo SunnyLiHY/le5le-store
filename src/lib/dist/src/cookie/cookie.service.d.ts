85619d4ed86ee1ac4cda320928375243b5b1cf5b
Alsmile <alsmile123@qq.com>
2017-12-06 15:35:01 +0800

    ng5 lib

--- src/lib/dist/src/cookie/cookie.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class CookieService {
-    static get(name: string): string;
-    static set(name: string, value: string, options?: any): void;
-    static delete(name: string, options?: any): void;
-}
--- src/lib/dist/src/lib.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export declare class Le5leStoreModule {
-    constructor(parentModule: Le5leStoreModule);
-    static forRoot(): ModuleWithProviders;
-}
